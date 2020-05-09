56103dedbf45eb1a77bd928cda2ff26707d0c4c8
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-05-09 20:17:46 +0200

    Data | PostgreSQL
    - Added Stories DB Context
    - Configuring ConnectionStrings
    - Added PostgreSQL inside Docker Compose

---
+++ Stories/Data/StoriesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stories.Models;
+
+namespace Stories.Data
+{
+    public class StoriesContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+
+        public StoriesContext(DbContextOptions<StoriesContext> options) : base(options)
+        {
+        }
+    }
+}
