cdb87e7d97ef2214374ee1487976e96fd054417a
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-05-10 01:43:20 +0200

    Models | Added Stories Type Field

--- Stories/Migrations/20200509234232_AddTypeToStoriesTable.Designer.cs
+++ Stories/Migrations/20200509234232_AddTypeToStoriesTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Stories.Data;
@@ -9,9 +10,10 @@ using Stories.Data;
 namespace Stories.Migrations
 {
     [DbContext(typeof(StoriesContext))]
-    partial class StoriesContextModelSnapshot : ModelSnapshot
+    [Migration("20200509234232_AddTypeToStoriesTable")]
+    partial class AddTypeToStoriesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Stories/Models/Story.cs
+++ Stories/Models/Story.cs
@@ -6,6 +6,7 @@ namespace Stories.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Type { get; set; }
         public string Description { get; set; }
         public string Data { get; set; }
         public Project Project { get; set; }
