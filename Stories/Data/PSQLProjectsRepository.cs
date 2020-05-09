8c4e913497d4c740aa3bdc98af56e75e90f7b267
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-05-09 20:27:12 +0200

    Data | PSQLProjectsRepository

---
+++ Stories/Data/PSQLProjectsRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Stories.Models;
+
+namespace Stories.Data
+{
+    public class PSQLProjectsRepository : IProjectRepository
+    {
+        private readonly StoriesContext _context;
+
+        public PSQLProjectsRepository(StoriesContext context)
+        {
+            _context = context;
+        }
+
+        public bool Store()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public IEnumerable<Project> Index()
+        {
+            return _context.Projects.ToList();
+        }
+
+        public Project Show(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Create(Project project)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Project project)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Project project)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
