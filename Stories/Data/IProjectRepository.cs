ce02a51fbe012f827d8423c389ef3a215b7c724b
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-05-09 19:58:45 +0200

    Repository | IProjectRepository

---
+++ Stories/Data/IProjectRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Stories.Models;
+
+namespace Stories.Data
+{
+    public interface IProjectRepository
+    {
+        bool Store();
+        IEnumerable<Project> Index();
+        Project Show(int id);
+        void Create(Project project);
+        void Update(Project project);
+        void Delete(Project project);
+    }
+}
