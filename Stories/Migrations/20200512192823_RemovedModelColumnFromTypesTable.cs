b87eeb7dbcbaaf50196479cba12649602f6e8c07
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-05-12 21:29:35 +0200

    Model | Types

---
+++ Stories/Migrations/20200512192823_RemovedModelColumnFromTypesTable.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stories.Migrations
+{
+    public partial class RemovedModelColumnFromTypesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Types");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Types",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
