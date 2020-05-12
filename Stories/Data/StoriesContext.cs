f82214bdec7912e4e58120d7dbc9404aec2434c0
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-05-13 01:32:20 +0200

    Model | Migrations

--- Stories/Data/StoriesContext.cs
+++ Stories/Data/StoriesContext.cs
@@ -8,6 +8,7 @@ namespace Stories.Data
         public DbSet<Project> Projects { get; set; }
         public DbSet<Story> Stories { get; set; }
         public DbSet<Type> Types { get; set; }
+        public DbSet<State> State { get; set; }
 
         public StoriesContext(DbContextOptions<StoriesContext> options) : base(options)
         {
---
+++ Stories/Models/State.cs
@@ -0,0 +1,8 @@
+﻿namespace Stories.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
