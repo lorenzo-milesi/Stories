a8a17edd6a7fa91a2575afa7b600995b7500a621
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-05-13 08:08:54 +0200

    Relation | Story HasMany BusinessRules

---
+++ Stories/Migrations/20200513060752_AddedStoryToBusinessRulesTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stories.Migrations
+{
+    public partial class AddedStoryToBusinessRulesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StoryId",
+                table: "BusinessRules",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BusinessRules_StoryId",
+                table: "BusinessRules",
+                column: "StoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BusinessRules_Stories_StoryId",
+                table: "BusinessRules",
+                column: "StoryId",
+                principalTable: "Stories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BusinessRules_Stories_StoryId",
+                table: "BusinessRules");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BusinessRules_StoryId",
+                table: "BusinessRules");
+
+            migrationBuilder.DropColumn(
+                name: "StoryId",
+                table: "BusinessRules");
+        }
+    }
+}
--- Stories/Models/BusinessRule.cs
+++ Stories/Models/BusinessRule.cs
@@ -4,5 +4,7 @@
     {
         public int Id { get; set; }
         public string Description { get; set; }
+        public Story Story { get; set; }
+        public int StoryId { get; set; }
     }
 }
